5509bf111bb9f3074e880e73305b62210201c883
bhavikmaradiya <68755759+bhavikmaradiya@users.noreply.github.com>
2020-11-08 10:11:33 +0530

    new

--- app/src/main/java/shravan/nyshadh/billmaker/SwipableViewPager.java
+++ app/src/main/java/shravan/nyshadh/billmaker/SwipableViewPager.java
@@ -8,19 +8,24 @@ import androidx.viewpager.widget.ViewPager;
 
 public class SwipableViewPager extends ViewPager {
     private boolean canScroll = false;
+
     public SwipableViewPager(Context context) {
         super(context);
     }
+
     public SwipableViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
+
     public void setCanScroll(boolean canScroll) {
         this.canScroll = canScroll;
     }
+
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         return canScroll && super.onTouchEvent(ev);
     }
+
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         return canScroll && super.onInterceptTouchEvent(ev);
